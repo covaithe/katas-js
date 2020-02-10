5194f329b0b07eaa0f5498e16edb961f650515e3
Greg Fleming <gfleming@coletaylor.com>
2020-02-10 08:57:19 -0500

    FAIL src/word-search.spec.js   word search     ○ skipped should find all the words in the sample puzzle     createPuzzle       ✓ should be a list of cells with value and coordinates (3ms)     candidatesFor       ✕ should find candidates starting at all cells with value matching word start (6ms)
      ● word search › candidatesFor › should find candidates starting at all cells with value matching word start
    
        expect(received).toEqual(expected) // deep equality
    
        - Expected
        + Received
    
          Array [
            Object {
        -     "path": "(0,0),(1,0)",
        -     "word": "ab",
        +     "path": "(0,0)",
        +     "word": "a",
            },
            Object {
        -     "path": "(0,2),(1,2)",
        -     "word": "ac",
        +     "path": "(0,2)",
        +     "word": "a",
            },
          ]
    
          57 |         a,c
          58 |       `))
        > 59 |       expect(candidatesFor(puzzle, 'ab')).toEqual([
             |                                           ^
          60 |         { word: 'ab', path: '(0,0),(1,0)' },
          61 |         { word: 'ac', path: '(0,2),(1,2)' },
          62 |       ])
    
          at Object.it (src/word-search.spec.js:59:43)
    
    Test Suites: 1 failed, 1 total
    Tests:       1 failed, 1 skipped, 1 passed, 3 total
    Snapshots:   0 total
    Time:        0.916s, estimated 1s
    Ran all test suites.

--- src/word-search.js
+++ src/word-search.js
@@ -4,7 +4,22 @@ const createPuzzle = input =>
     .flatMap((line, y) => line.split(",")
       .map((value, x) => ({ value, x, y}) ))
 
-const candidatesFor = () => {}
+const createCandidate = cells => {
+  return {
+    word: cells[0].value,
+    path: `(${cells[0].x},${cells[0].y})`
+  }
+}
+const candidatesFor = (puzzle, word) => {
+  const starts = puzzle.filter(cell => cell.value === word[0])
+  return starts.map(start => {
+    const cells = [
+      start,
+      puzzle.find(c => c.x === start.x+1 && c.y === start.y)
+    ]
+    return createCandidate(cells)
+  })
+}
 
 const find = () => {}
 
